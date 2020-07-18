239d633691b483f6655a61106b0caff816ce0a94
Roman Roberto <romanqramirez@gmail.com>
2020-07-17 20:29:05 -0700

    updated resume and sitemap titles

--- Assignments/week_5/javascript/scripts/main.js
+++ Assignments/week_5/javascript/scripts/main.js
@@ -1,2 +1,20 @@
-console.log("Hello, world!");
-var myFirstNumber = 100;
+// If Statement
+
+var number1 = 10;
+var number2 = 15;
+
+
+
+
+// Syntax
+// if (conditional) { group of code to run if conditional is true }
+
+
+// Logical Operators
+// == compares two values, becomes the  boolean true if it is the same, and false if they are not true.
+
+if (number1 ==10) {
+    console.log("This if statement was true so i ran this code!");
+}
+
+console.log("Program Finished!");
